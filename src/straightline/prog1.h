a6fe53be8fe23d7015e9564289a97451a661e8a2
Jianbang Yang <yangjianbang112@gmail.com>
2019-09-05 09:57:00 +0800

    [Lab1] Add lab1 files

---
+++ src/straightline/prog1.h
@@ -0,0 +1,12 @@
+#ifndef STRAIGHTLINE_PROG1_H_
+#define STRAIGHTLINE_PROG1_H_
+
+#include "straightline/slp.h"
+
+A::Stm *prog(void);
+A::Stm *prog(void);
+A::Stm *prog_prog(void);
+A::Stm *right_prog(void);
+A::Stm *error_prog(void);
+
+#endif  // STRAIGHTLINE_PROG1_H_
